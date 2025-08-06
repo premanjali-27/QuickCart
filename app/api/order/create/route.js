dbc289074aae8d2895a8c5edfbfb3762460056d9
premanjali-27 <preminarayana944@gmail.com>
2025-08-06 20:00:22 +0530

    Finished project

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -19,7 +19,7 @@ export async function POST(request) {
         // calculate amount using items
         const amount = items.reduce(async (acc, item) => {
             const product = await Product.findById(item.product);
-            return acc + product.offerPrice * item.quantity;
+            return await acc + product.offerPrice * item.quantity;
         }, 0)
 
         await inngest.send({
